61f9452bd84b0ea73261fbdb5be508baf6e91a1b
Sebastian Braun <sebastian@elmnt.de>
2020-04-05 10:59:40 +0200

    bugfixes

--- coverage-service.py
+++ coverage-service.py
@@ -1,56 +1,58 @@
 #!/usr/bin/env python
 
 import argparse
+import logging
 import os
-import socketserver
 
 from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+from tempfile import NamedTemporaryFile
 
 import coverage
 
 
-class Handler(BaseHTTPRequestHandler):
+logger = logging.getLogger(__name__)
+
+
+class Server(HTTPServer):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.report = None
+        self.report = coverage.CoverageData()
+        self.report.write()
+
+
+class Handler(BaseHTTPRequestHandler):
 
     def do_HEAD(self):
         self.send_response(HTTPStatus.OK)
         self.send_header("Content-type", "application/octet-stream")
+        self.send_header("Content-Length", str(os.path.getsize(self.server.report.base_filename())))
         self.end_headers()
 
     def do_GET(self):
-        if self.report is None:
-            self.report = coverage.CoverageData(no_disk=True)
-        data = self.report.dumps()
-
-        self.send_response(HTTPStatus.OK)
-        self.send_header("Content-type", "application/octet-stream")
-        self.send_header("Content-Length", str(len(data)))
-        self.end_headers()
-
-        self.wfile.write(data)
+        self.do_HEAD()
+        with open(self.server.report.base_filename(), 'rb') as f:
+            self.wfile.write(f.read())
 
     def do_DELETE(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-
-        self.report = None
+        self.server.report.erase()
 
     def do_PUT(self):
         length = int(self.headers['Content-Length'])
         data = self.rfile.read(length)
 
-        report = coverage.CoverageData(no_disk=True)
-
-        if self.report is None:
-            self.report = report
-        else:
-            self.report.update(report)
+        with NamedTemporaryFile() as fp:
+            fp.write(data)
+            fp.flush()
+            report = coverage.CoverageData(basename=fp.name)
+            self.server.report.update(report)
 
         self.send_response(HTTPStatus.OK)
+        self.end_headers()
 
 
 if __name__ == '__main__':
@@ -61,5 +63,5 @@ if __name__ == '__main__':
                         help='Specify port [default: 8000]')
     args = parser.parse_args()
 
-    with socketserver.TCPServer(("", args.port), Handler) as httpd:
+    with Server(('', args.port), Handler) as httpd:
         httpd.serve_forever()
